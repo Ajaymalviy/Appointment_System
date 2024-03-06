352497b93fe62fd2fe785c004998d7e205d5b5f9
AjayMalviya <ajeymalviya143@gmail.com>
2024-03-06 23:24:32 +0530

    making changes in model file ,which is changing the order of class based function in models file and change employee collection place at top due to not having a foreign key

--- main_file_project/first_app_for_project/models.py
+++ main_file_project/first_app_for_project/models.py
@@ -8,12 +8,37 @@ from django.db import models
 #   addition_info = fields.DictField()
 
 
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+
+
+
+class Employee(models.Model):
+    employee_email = models.EmailField()
+    employee_role = models.CharField(max_length=100)
+    employee_name = models.CharField(max_length=100)
+    experience = models.IntegerField()
+    skills = models.JSONField()
+
 class Company(models.Model):
     company_name = models.CharField(max_length=100)
     employee_email = models.EmailField()
     email = models.EmailField()
     location = models.CharField(max_length=100)
     company_schedule = models.CharField(max_length=100)
+
+    
+class Schedule(models.Model):
+    schedule_id = models.CharField(max_length=100)
+    date = models.DateField()
+    number_of_slots = models.IntegerField()
+    available = models.BooleanField()
+    employee_email = models.EmailField()
+
 
 class Appointment(models.Model):
     appointment_id = models.CharField(max_length=100)
@@ -25,25 +50,8 @@ class Appointment(models.Model):
     user_email = models.CharField(max_length=100)
     schedule_id = models.CharField(max_length=100)
 
-class Employee(models.Model):
-    employee_email = models.EmailField()
-    employee_role = models.CharField(max_length=100)
-    employee_name = models.CharField(max_length=100)
-    experience = models.IntegerField()
-    skills = models.JSONField()
 
-class Schedule(models.Model):
-    schedule_id = models.CharField(max_length=100)
-    date = models.DateField()
-    number_of_slots = models.IntegerField()
-    available = models.BooleanField()
-    employee_email = models.EmailField()
 
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
 
 from django.db import connections
 from passlib.hash import sha256_crypt
