0b667b2784dde4e7415e2d3aa27e76f93b807875
AjayMalviya <ajeymalviya143@gmail.com>
2024-03-07 22:25:45 +0530

    same url in main project

--- main_file_project/first_app_for_project/urls.py
+++ main_file_project/first_app_for_project/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
-from first_app_for_project.views import index
+from first_app_for_project.views import index,getting_data
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('', index, name='about'),
 ]
@@ -11,5 +12,6 @@ from .views import home, login
 
 urlpatterns = [
     path('/home', home, name='home'),
+    path('/search', getting_data, name='getting_data'),
     # Other URL patterns for your application
 ]
