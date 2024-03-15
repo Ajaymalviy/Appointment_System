72596fe4592f7a803e842cf5dcba708de5b4e441
AjayMalviya <ajeymalviya143@gmail.com>
2024-03-15 23:15:08 +0530

    making changes into the file of view and rearrange the fuction with its calling

--- main_file_project/first_app_for_project/views.py
+++ main_file_project/first_app_for_project/views.py
@@ -1,22 +1,71 @@
-from django.http import HttpResponse
 
+
+
+'''
+Basically this is the file of python which contain many fucntion of python which redirect,renders on some other pages.
+
+below are the listing of subfucntion.
+
+index: Renders the "about.html" template
+which is our first page.
+
+register_user: Handles user registration. 
+When a POST request is received, it takes username, password, email,
+and phone number from the request, hashesing of password,
+creates a new object of user, and saves it to the database. Then, redirects to the login page.
+
+login: Handles user login.  
+it extract the username and password from the request, 
+and checks if the password matches correctly it renders the "index.html"
+template; otherwise, it renders the "login_page.html" template with an error message.
+
+home: Renders the "index.html" template, which i create for logo of my website.
+
+get_company_data: taking company data from a database collection based on the provided company_name. 
+and return the data as JSON response or may  renders the "services.html" template with the company_name and employee details.
+
+rating: Renders the "rating.html" template, which i used for give the rating to by user
+
+logout: this functin is used for just logout the basic session, "about.html" template.
+
+takeschedule: Renders the "takeschedule.html" template, for taking the request for empty schedule by on the end of employee.
+
+save_request_for_meeting: Saves meeting requests to a MongoDB collection. It extracts email, description, and date from the request, creates a document, inserts it into the collection, and renders the "success_page.html" template.
+
+'''
+
+
+import json
+from bson import ObjectId 
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render,redirect
-from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import check_password
+from django.contrib.auth import logout as auth_logout
 from .models import User
+from django.contrib.auth.hashers import make_password
+from django.urls import reverse
+import pymongo
+from pymongo import MongoClient
+from datetime import datetime
 
-
-
-# def index(request):
-#     return render(request, 'index.html')
-
-
-#--------------------------------Login------------------------------------------------------------
 
 
 def index(request):
     return render(request, 'about.html')
 
+def register_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        hashed_password = make_password(password)
+        user = User(username=username, password=hashed_password, 
+                    email=email, phone=phone
+        )
+        user.save()
+        return redirect(reverse('login'))  
+    return render(request, 'registration.html')
 
 def login(request):
     if request.method == 'POST':
@@ -31,56 +80,28 @@ def login(request):
   
         if check_password(password, user.password):
             print('success')
+            return render(request, 'index.html')
         else:
-            return render(request, 'login_page.html', {'error': 'Invalid username or password'})  
+            return render(request, 'login_page.html', 
+                        {'error':'Invalid username or password' })  
     return render(request, 'login_page.html')
 
-#------------------------------------Registration--------------------------------------------
-from django.shortcuts import render, redirect
-from django.contrib.auth.hashers import make_password
-from django.urls import reverse
-
-def register_user(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        hashed_password = make_password(password)
-        user = User(username=username, password=hashed_password, email=email, phone=phone)
-        user.save()
-        return redirect(reverse('login'))  
-
-    return render(request, 'registration.html')
-
-#-----------------------------------------------------home after login-----------
 
 def home(request):
     return render(request, 'index.html')
 
-#--------------------search-company------------------------
-from django.http import JsonResponse
-import pymongo
-import json
-from bson import ObjectId 
-from django.shortcuts import render
-
 def get_company_data(request):
-
     if request.method != 'POST':
-        return JsonResponse({'error': 'Invalid request method. Use POST to retrieve company data.'})
-
+        return JsonResponse({'error': 'Invalid request method. Use POST to retrieve company data.'
+        })
     company_name = request.POST.get('company_name')
-
     if not company_name:
         return JsonResponse({'error': 'Invalid company name'})
-
     try:
         client = pymongo.MongoClient('mongodb://localhost:27017/')  
         db = client['meetme'] 
         collection = db['Employee']  
         employees = collection.find({'company_name': company_name})
-
         data_list = []
         for employee in employees:
             data_list.append({
@@ -88,36 +109,25 @@ def get_company_data(request):
                 'skills': employee['skills'],
                 'experience': str(employee['experience']) +'-Yr'
             })
-
         if data_list:
-            return render(request, 'services.html', {'company_name': company_name, 'employees': data_list})
+            return render(request, 'services.html', {
+                'company_name': company_name, 'employees': data_list
+            })
         else:
-            return JsonResponse({'error': 'No details found for the selected company'})
-
+            return JsonResponse({'error':'No details found for the selected company'
+            })
     except Exception as e:
         return JsonResponse({'error': f'An error occurred: {str(e)}'})
-    
-#---------------------rating page call-----------------------------------
     
 def rating(request):
     return render(request, 'rating.html')    
 
-#------------------------logout---------------------------------------------
-
-from django.contrib.auth import logout as auth_logout
 def logout(request):
-    auth_logout(request)  # Clear session and log out the user
+    auth_logout(request)  
     return render(request, 'about.html') 
-
-#__________takescheduleby___________user-------------------------
 
 def takeschedule(request):
     return render(request, 'takeschedule.html')   
-
-#---------------------Request_For_Schedule--------------------------method------------------
-from django.shortcuts import render
-from pymongo import MongoClient
-from datetime import datetime
 
 def save_request_for_meeting(request):
     if request.method == 'POST':
@@ -134,11 +144,10 @@ def save_request_for_meeting(request):
             'description': description,
             'date': date
         }
+
         collection.insert_one(request_document)
         client.close()
         return render(request, 'success_page.html')
-    
-    # If the request method is not POST, render the form template
     return render(request, 'takeshcedule.html')
 
 
