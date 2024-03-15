97bc0bf1aca7e28d3aefa98e7fcbabfddd3a84d3
AjayMalviya <ajeymalviya143@gmail.com>
2024-03-15 23:10:29 +0530

    just change the pattern of url

--- main_file_project/first_app_for_project/urls.py
+++ main_file_project/first_app_for_project/urls.py
@@ -3,14 +3,10 @@ from django.urls import path,include
 from first_app_for_project.views import index,getting_data
 from django.conf import settings
 from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', index, name='about'),
-]
-from django.urls import path
 from .views import get_company_data, home, login
 
 urlpatterns = [
+    path('', index, name='about'),
     path('/home', home, name='home'),
     path('/search', get_company_data, name='getting_company_data'),
     path('meeting_request/', views.save_request_for_meeting, name='save_request_for_meeting'),
