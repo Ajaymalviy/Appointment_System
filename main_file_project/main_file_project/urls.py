02633b76726a52af8a9d7551405ca2ca8e51cbc5
AjayMalviya <ajeymalviya143@gmail.com>
2024-03-24 10:30:55 +0530

    making changes into urls for new urls addtion

--- main_file_project/main_file_project/urls.py
+++ main_file_project/main_file_project/urls.py
@@ -14,21 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path
 from first_app_for_project.views import get_company_data, index,home
 from django.conf import settings
 from django.conf.urls.static import static
 from first_app_for_project import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-
-urlpatterns = [
     path('', views.index, name='about'),
-    path('login/', views.login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('rating/', views.rating, name='rating'),
     path('register/', views.register_user, name='register'),
