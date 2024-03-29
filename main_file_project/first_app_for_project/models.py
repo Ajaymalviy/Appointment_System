7b667286e29d4432c7d01095e6293c81668df442
AjayMalviya <ajeymalviya143@gmail.com>
2024-03-29 22:55:03 +0530

    change model file for migrate again for new addition

--- main_file_project/first_app_for_project/models.py
+++ main_file_project/first_app_for_project/models.py
@@ -1,17 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     email = models.EmailField()
-    phone = models.CharField(max_length=15)
-
-
+  
 
 class Company(models.Model):
     company_name = models.CharField(max_length=100)
-    company_email = models.EmailField()
+    company_email = models.EmailField(default='xyz@gmail.com')
     location = models.CharField(max_length=100)
     company_schedule = models.CharField(max_length=100)
     def __str__(self):
@@ -25,8 +23,10 @@ class Employee(models.Model):
     experience = models.IntegerField()
     skills = models.JSONField()
     company = models.ForeignKey('Company', on_delete=models.CASCADE) 
-
-
+    password = models.CharField(max_length=50 , default='ajay')
+    def __str__(self):
+        return self.employee_email
+    
     
 class Schedule(models.Model):
     schedule_id = models.CharField(max_length=100)
@@ -34,7 +34,9 @@ class Schedule(models.Model):
     number_of_slots = models.IntegerField()
     available = models.BooleanField()
     employee_email = models.EmailField()
-
+    def __str__(self):
+        return self.schedule_id
+    
 
 class Appointment(models.Model):
     appointment_id = models.CharField(max_length=100)
@@ -46,26 +48,7 @@ class Appointment(models.Model):
     user_email = models.CharField(max_length=100)
     schedule_id = models.CharField(max_length=100)
 
-
-
-
-from django.db import connections
-from passlib.hash import sha256_crypt
-
-def authenticate_user(username, password):
-    # Get the MongoDB connection from Django's ORM
-    db_connection = connections['phone']
-
-    # Access the MongoDB database and collection
-    db = db_connection.get_database('phone')  # Specify the database name
-    collection = db['user']  # Specify the collection name
-
-    # Find user by username
-    user = collection.find_one({'username': username})
-
-    if user:
-        # Check if password matches
-        if sha256_crypt.verify(password, user['password']):
-            return True, user
-    return False, None
+    def __str__(self):
+        return self.appointment_id
+    
 
