42e3df7997d4b1cb6c8f06517d62c716c6b65d1d
AjayMalviya <ajeymalviya143@gmail.com>
2024-04-04 21:41:50 +0530

    changing the database path place at above the template and checking to run manual change

--- main_file_project/main_file_project/settings.py
+++ main_file_project/main_file_project/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
