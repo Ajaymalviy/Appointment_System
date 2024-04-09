fd139de2f5f5b5c0ba1678d06d25f426480e9785
AjayMalviya <ajeymalviya143@gmail.com>
2024-04-09 21:14:02 +0530

    making changes into models as making or changes the primary key email in user field and email in same employee and also the concept of foreign key into model changing and generate according to this new data

--- main_file_project/first_app_for_project/models.py
+++ main_file_project/first_app_for_project/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(primary_key=True)
   
 
 class Company(models.Model):
@@ -17,38 +17,48 @@ class Company(models.Model):
     
 
 class Employee(models.Model):    
-    employee_email = models.EmailField()
+    employee_email = models.EmailField( primary_key=True)
     employee_role = models.CharField(max_length=100)
     employee_name = models.CharField(max_length=100)
     experience = models.IntegerField()
     skills = models.JSONField()
-    company = models.ForeignKey('Company', on_delete=models.CASCADE) 
+    company_name = models.ForeignKey('Company', on_delete=models.CASCADE) 
     password = models.CharField(max_length=50 , default='ajay')
     def __str__(self):
         return self.employee_email
     
     
 class Schedule(models.Model):
-    schedule_id = models.CharField(max_length=100)
+    schedule_id = models.CharField(primary_key=True,max_length=100)
     date = models.DateField()
     number_of_slots = models.IntegerField()
     available = models.BooleanField()
-    employee_email = models.EmailField()
+    employee_email = models.ForeignKey('Employee', on_delete=models.CASCADE) 
     def __str__(self):
         return self.schedule_id
     
 
 class Appointment(models.Model):
-    appointment_id = models.CharField(max_length=100)
+    appointment_id = models.CharField(primary_key=True, max_length=100)
     date = models.DateField()
     time = models.CharField(max_length=100)
     description = models.TextField()
     status = models.CharField(max_length=100)
-    employee_email = models.EmailField()
-    user_email = models.CharField(max_length=100)
-    schedule_id = models.CharField(max_length=100)
+    employee_email = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    email = models.ForeignKey(User, on_delete=models.CASCADE)
+    schedule_id = models.ForeignKey(Schedule, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.appointment_id
+
     
 
+
+class MeetingRequest(models.Model):
+    employee_email = models.EmailField()
+    requester_email = models.EmailField()
+    description = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f"Meeting Request for {self.employee_email} on {self.date}"
