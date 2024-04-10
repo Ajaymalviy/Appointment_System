2e9536b7d58ad6297675c51e84165f10bb66ae93
AjayMalviya <ajeymalviya143@gmail.com>
2024-04-10 20:52:06 +0530

    adding new class for auth permission to admin is that meeting request model with its list

--- main_file_project/first_app_for_project/admin.py
+++ main_file_project/first_app_for_project/admin.py
@@ -2,26 +2,32 @@ from django.contrib import admin
 
 # Register your models here.
 
-from first_app_for_project.models import Employee, Schedule, User, Company, Appointment
+from first_app_for_project.models import Employee, Schedule, User, Company, Appointment,MeetingRequest
 
 
-
-class ScheduleData(admin.ModelAdmin) :
-
-    list_display =['schedule_id','date','number_of_slots','available','employee_email']
 
 class Userdata(admin.ModelAdmin):
     list_display = ['username','password','email']
 
 class Companydata(admin.ModelAdmin):
-    list_display = ['company_name','company_email','location','company_schedule']
+    list_display = ('company_name', 'company_email', 'location', 'company_schedule')
+
+class Employeedata(admin.ModelAdmin):
+    list_display = ('employee_email', 'employee_role', 'employee_name', 'experience', 'company_name')
+
+class ScheduleData(admin.ModelAdmin) :
+    list_display = ('schedule_id', 'date', 'number_of_slots', 'available', 'employee_email')
 
 class Appointmentdata(admin.ModelAdmin):
-    list_display = ['appointment_id','date','time','description','status','employee_email','user_email','schedule_id',]
+    list_display = ('appointment_id', 'date', 'time', 'description', 'status', 'employee_email', 'email', 'schedule_id')
 
-admin.site.register(Employee)
-admin.site.register(Schedule,ScheduleData)
+class MeetingRequestdata(admin.ModelAdmin):
+    list_display = ('employee_email', 'requester_email', 'description', 'date')
+
 admin.site.register(User,Userdata)
 admin.site.register(Company,Companydata)
+admin.site.register(Employee,Employeedata)
+admin.site.register(Schedule,ScheduleData)
 admin.site.register(Appointment,Appointmentdata)
+admin.site.register(MeetingRequest,MeetingRequestdata)
 
