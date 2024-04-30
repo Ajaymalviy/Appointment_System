f695aa4ac1c47d8b6c5823f41d4c36364efd8319
AjayMalviya <ajeymalviya143@gmail.com>
2024-04-30 23:16:11 +0530

    adding new url same as with main of url

--- main_file_project/first_app_for_project/urls.py
+++ main_file_project/first_app_for_project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from first_app_for_project.views import index,getting_data
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import get_company_data, home, login
+from .views import get_company_data, home, login, sendmail
 
 from django.contrib import admin
 
@@ -13,5 +13,6 @@ urlpatterns = [
     path('/home', home, name='home'),
     path('/search', get_company_data, name='getting_company_data'),
     path('meeting_request/', views.save_request_for_meeting, name='save_request_for_meeting'),
-  
+    
+
 ]
