3eacd19e504540e6c48e194285bcf7175576ff3f
AjayMalviya <ajeymalviya143@gmail.com>
2024-05-06 15:44:51 +0530

    makes chagnes into urls file where i changed add new link

--- main_file_project/main_file_project/urls.py
+++ main_file_project/main_file_project/urls.py
@@ -16,23 +16,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from first_app_for_project.views import get_company_data, index,home
+from first_app_for_project.views import index, aboutpage,servicepage, contactpage,loginnew,registernew, user_login,employee_login,back, employee_registration, meeting
 from django.conf import settings
 from django.conf.urls.static import static
 from first_app_for_project import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='about'),
+    path('', views.index, name='index'),
+    path('about/', views.aboutpage, name='aboutpage'),
+    path('service/', views.servicepage, name='servicepage'),
+    path('contact/', views.contactpage, name='contactpage'),
+    path('loginnew/', views.loginnew, name='loginnew'),
+    path('registernew/', views.registernew, name='registernew'),
     path('user_login/', views.user_login, name='user_login'),
     path('employee_registration/', views.employee_registration, name='employee_registration'),
     path('employee_login/', views.employee_login, name='employee_login'),
     path('logout/', views.logout, name='logout'),
-    path('rating/', views.rating, name='rating'),
-    path('register/', views.register_user, name='register'),
-    path('home/', home, name='home'),
-    path('search/',views.get_company_data, name='getting_company_data'),
+    path("meeting_home/", views.meeting,  name="meeting"),
+    path('search/', views.search_company_view, name='search_company'),
     path('meeting_request/', views.save_request_for_meeting, name='meeting_request'),
     path('sendmail/', views.sendmail, name='sendmail'),
+    path('home/', views.back, name="back")
 ]
 
