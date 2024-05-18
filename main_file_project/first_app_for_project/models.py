a7b3edf54e8babc518cbfffe040dd6c1aa6b9c38
AjayMalviya <ajeymalviya143@gmail.com>
2024-05-18 23:34:34 +0530

    added new class of contact which takes data entry from form and put into database

--- main_file_project/first_app_for_project/models.py
+++ main_file_project/first_app_for_project/models.py
@@ -59,7 +59,16 @@ class MeetingRequest(models.Model):
 
     def __str__(self):
         return f"Meeting Request for {self.employee_email} on {self.date}"
-    
 
+
+class ContactDetail(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name + " - " + self.email
 
 
