b8f61fe89343ed1ebd93831877dbd749429042e1
AjayMalviya <ajeymalviya143@gmail.com>
2024-05-24 18:08:34 +0530

    added new app of application

--- main_file_project/main_file_project/wsgi.py
+++ main_file_project/main_file_project/wsgi.py
@@ -12,5 +12,10 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_file_project.settings')
+# print('goodydm')
+
 
 application = get_wsgi_application()
+# print(application)
+app = application
+
