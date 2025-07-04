0585f86ff0ccf2808b974ee9af677dd758bd0a06
t <ajeymalviya143@gmail.com>
2025-07-04 22:59:17 +0530

    updated project structure for unwanted directories and files

--- main_file_project/first_app_for_project/dell.py
+++
@@ -1,9 +0,0 @@
-from .models import User, Company, Employee, Schedule, Appointment, MeetingRequest
-
-# Delete all existing data from each model
-User.objects.all().delete()
-Company.objects.all().delete()
-Employee.objects.all().delete()
-Schedule.objects.all().delete()
-Appointment.objects.all().delete()
-MeetingRequest.objects.all().delete()
